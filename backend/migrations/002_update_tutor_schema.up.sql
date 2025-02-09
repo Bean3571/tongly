7b6c12533b0165d010e492383d6cf947c27a1da3
Bean3571 <67775683+Bean3571@users.noreply.github.com>
2025-02-09 22:54:10 +0300

    Merged migrations, updated gitignore

--- backend/migrations/002_update_tutor_schema.up.sql
+++
@@ -1,5 +0,0 @@
--- Drop native_languages column from tutor_details
-ALTER TABLE tutor_details DROP COLUMN native_languages;
-
--- Rename degrees to education
-ALTER TABLE tutor_details RENAME COLUMN degrees TO education; 
