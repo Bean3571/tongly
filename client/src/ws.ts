fdadc27ca0e8f4836821962b07e40d75293680ee
Bean3571 <67775683+Bean3571@users.noreply.github.com>
2025-04-12 20:26:12 +0300

    clearup

--- client/src/ws.ts
+++
@@ -1,6 +0,0 @@
-import socketIOClient from "socket.io-client";
-import { SOCKET_URL } from "./config";
-
-// Use network IP from config instead of hard-coded value
-export const WS = SOCKET_URL;
-export const ws = socketIOClient(WS);
